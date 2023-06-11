8d88131e9da1258d8524c215d7209a2e5aea386e
antuv <antu.villegas96@gmail.com>
2023-06-10 22:17:33 -0300

    * Corregidas constantes de tipos de notificación * Versión 1.2.1

--- src/Constantes/Notifications.php
+++ src/Constantes/Notifications.php
@@ -4,8 +4,8 @@ namespace TresErres\NotisConector\Constantes;
 
 final class Notifications
 {
-    public const NOTIFICATION_TYPE_PERSONAL_PEDIDO_REVISION = 'personal:registro-firmado-actualizado';
-    public const NOTIFICATION_TYPE_PERSONAL_REGISTRO_FIRMADO_ACTUALIZADO = 'personal:pedido-revision';
+    public const NOTIFICATION_TYPE_PERSONAL_PEDIDO_REVISION = 'personal:pedido-revision';
+    public const NOTIFICATION_TYPE_PERSONAL_REGISTRO_FIRMADO_ACTUALIZADO = 'personal:registro-firmado-actualizado';
 
     public const NOTIFICATION_TYPES = [
         self::NOTIFICATION_TYPE_PERSONAL_PEDIDO_REVISION,
